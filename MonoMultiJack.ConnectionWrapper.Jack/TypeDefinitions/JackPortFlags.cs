047f21b6f6775ba6f70a88a7946fba8e98cb07b0
Thomas Mayer <thomas@residuum.org>
2012-06-08 00:33:31 +0200

    Refactoring wrapper for Jackd.

--- MonoMultiJack.ConnectionWrapper.Jack/TypeDefinitions/JackPortFlags.cs
+++ MonoMultiJack.ConnectionWrapper.Jack/TypeDefinitions/JackPortFlags.cs
@@ -1,10 +1,10 @@
 // 
-// LibJackWrapperClasses.cs
+// JackPortFlags.cs
 //  
 // Author:
 //       Thomas Mayer <thomas@residuum.org>
 // 
-// Copyright (c) 2010 Thomas Mayer
+// Copyright (c) 2012 Thomas Mayer
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -24,21 +24,17 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
-
 namespace MonoMultiJack.ConnectionWrapper.Jack
 {
-    /// <summary>
-    /// Wrapper class for libjack. This file contains definitions for private classes, enums and constants.
-    /// </summary>
-    internal static partial class LibJackWrapper
-    {		
-	private const string JACK_LIB_NAME = "libjack.so.0";
-	private const string JACK_DEFAULT_AUDIO_TYPE = "32 bit float mono audio";
-	private const string JACK_DEFAULT_MIDI_TYPE = "8 bit raw midi";		
-	private delegate void JackPortRegistrationCallback (uint port,int register,IntPtr args);
-
-	private delegate void JackPortConnectCallback (int a,int b,int connect,IntPtr args);
-
-	private delegate void JackShutdownCallback (IntPtr args);
-    }
+	[Flags]
+	internal enum JackPortFlags
+	{
+	    JackPortIsInput = 0x1,
+	    JackPortIsOutput = 0x2,
+	    JackPortIsPhysical = 0x4,
+	    JackPortCanMonitor = 0x8,
+	    JackPortIsTerminal = 0x10
+	}
+	
 }
+
