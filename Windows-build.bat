1fd2355e7263a167d45bd925459423983c33f15e
Thomas Mayer <thomas@residuum.org>
2014-08-20 16:13:46 +0200

    Windows compilation from bat
    - Clean
    - Compile
    - wait for keypress.

--- Windows-build.bat
+++ Windows-build.bat
@@ -1,3 +1,5 @@
 call git submodule init
 call git submodule update
+call %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe MonoMultiJack.Win.sln /t:Clean
 call %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe MonoMultiJack.Win.sln /p:Configuration=Release
+pause
