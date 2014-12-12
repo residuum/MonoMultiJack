47939a1c9206f82f17fee2fe13918cfca152755a
Thomas Mayer <thomas@residuum.org>
2014-12-12 02:21:43 +0100

    Make tar.bz2 after Linux build.

--- Linux-build.sh
+++ Linux-build.sh
@@ -3,3 +3,7 @@ git submodule init
 git submodule update
 xbuild MonoMultiJack.Lnx.sln /t:Clean
 xbuild MonoMultiJack.Lnx.sln /p:Configuration=Release
+cd MonoMultiJack.Linux/bin/Release
+tar -cf MonoMultiJack.tar *
+bzip2 MonoMultiJack.tar
+mv MonoMultiJack.tar.bz2 ../../../
