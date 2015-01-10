67f0a41ecc43aa548e74a5211b9ed7f3fb0efce3
Thomas Mayer <thomas@residuum.org>
2015-01-10 02:54:44 +0100

    Custom icons for dialogs.

--- MonoMultiJack.Mvc/Forms/Dialog.cs
+++ MonoMultiJack.Mvc/Forms/Dialog.cs
@@ -25,6 +25,8 @@
 // THE SOFTWARE.
 using System;
 using Xwt;
+using Xwt.Drawing;
+using Xwt.Formats;
 
 namespace MonoMultiJack.Forms
 {
@@ -32,12 +34,46 @@ namespace MonoMultiJack.Forms
 	{
 		public static void ShowErrorMessage (string message)
 		{
-			MessageDialog.ShowError (message);
+			//MessageDialog.ShowError (message);
+			DisplayMessage (message, Icons.Warning);
 		}
 
 		public static void ShowInfoMessage (string message)
 		{
-			MessageDialog.ShowMessage (message);
+			//MessageDialog.ShowMessage (message);
+			DisplayMessage (message, Icons.Info);
+		}
+
+		static void DisplayMessage(string message, Image icon){
+			Window window = new Window ();
+			window.Icon = icon;
+			VBox box = new VBox ();
+			box.PackStart (BuildMessage (message, icon));
+			HBox buttonBox = new HBox();
+			Button button = new Button{Label = "OK"};
+			button.Clicked += (sender, e) => {
+				window.Close();
+				window.Dispose();
+			};
+			buttonBox.PackEnd (button);
+			box.PackStart (buttonBox);
+			window.Content = box;
+			window.ShowInTaskbar = false;
+			window.Show ();
+			window.Present ();
+		}
+
+		static Widget BuildMessage(string message, Image icon){
+			HBox messageBox = new HBox ();
+			ImageView iconView = new ImageView (icon.WithSize (IconSize.Medium));
+			iconView.VerticalPlacement = WidgetPlacement.Start;
+			messageBox.PackStart(iconView);
+			RichTextView textView = new RichTextView ();
+			textView.LoadText (message, TextFormat.Markdown);
+			textView.MinWidth = 200;
+			textView.WidthRequest = 400;
+			messageBox.PackStart (textView);
+			return messageBox;
 		}
 	}
 }
