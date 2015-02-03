5f908a3aac1d4941d29cf1337e989d159f9218b2
Thomas Mayer <thomas@residuum.org>
2015-02-03 10:55:43 +0100

    Clean both solutions.

--- Linux-build.sh
+++ Linux-build.sh
@@ -2,19 +2,23 @@
 
 XBUILD=xbuild
 SOLUTION=MonoMultiJack.Lnx.sln
+OTHER=MonoMultiJack.Lnx.Gtk3.sln
+cp -f MonoMultiJack.Linux/MonoMultiJack.Linux.config MonoMultiJack.Linux/app.config
+
 if [ "$1" == "Gtk3" ]; then
 	SOLUTION=MonoMultiJack.Lnx.Gtk3.sln
+	OTHER=MonoMultiJack.Lnx.sln
+	cp -f MonoMultiJack.Linux/MonoMultiJack.Linux.Gtk3.config MonoMultiJack.Linux/app.config
 fi
+
 git submodule init
 git submodule update
+
 ${XBUILD} ${SOLUTION} /p:Configuration=Release /t:Clean
-#Not nice, but necessary because of https://bugzilla.xamarin.com/show_bug.cgi?id=18761
-if [ "$1" == "Gtk3" ]; then
-	cp -f MonoMultiJack.Linux/MonoMultiJack.Linux.Gtk3.config MonoMultiJack.Linux/app.config
-else
-	cp -f MonoMultiJack.Linux/MonoMultiJack.Linux.config MonoMultiJack.Linux/app.config
-fi
+${XBUILD} ${OTHER} /p:Configuration=Release /t:Clean
+
 ${XBUILD} ${SOLUTION} /p:Configuration=Release
+
 cd MonoMultiJack.Linux/bin/Release
 tar -cf MonoMultiJack.tar *
 bzip2 MonoMultiJack.tar
