85292e8b8c12dd1b56eea70ade0c0f89acaa0987
Thomas Mayer <thomas@residuum.org>
2015-08-17 12:44:07 +0200

    [WIN] Installer moved from vdproj to wixproj.

--- Windows-build.bat
+++ Windows-build.bat
@@ -5,9 +5,7 @@ CALL git submodule update --init
 CALL Windows-pocompile.bat
 CALL %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe MonoMultiJack.Win.sln /p:Configuration=%CONFIG% /t:Clean
 CALL %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe MonoMultiJack.Win.sln /p:Configuration=%CONFIG%
-CALL %VS_PATH% MonoMultiJack.Win.sln /build "%CONFIG%" /project MonoMultiJack.Windows.Setup\MonoMultiJack.Windows.Setup.vdproj /projectconfig "%CONFIG%"
 
-COPY MonoMultiJack.Windows.Setup\%CONFIG%\MonoMultiJack.Setup.msi MonoMultiJack.Setup.msi
-COPY MonoMultiJack.Windows.Setup\%CONFIG%\setup.exe MonoMultiJack-setup.exe
+COPY MonoMultiJack.Windows.Setup\bin\%CONFIG%\MonoMultiJack.msi MonoMultiJack.msi
 
 Pause
