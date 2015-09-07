06d6b9005da0abd4aa61398fdda4893ef247ef5c
Thomas Mayer <thomas@residuum.org>
2015-09-07 23:00:00 +0200

    Remove file copy from build script.

--- Linux-build.sh
+++ Linux-build.sh
@@ -3,12 +3,10 @@
 XBUILD=xbuild
 SOLUTION=MonoMultiJack.Lnx.sln
 OTHER=MonoMultiJack.Lnx.Gtk3.sln
-cp -f MonoMultiJack.Linux/MonoMultiJack.Linux.config MonoMultiJack.Linux/app.config
 
 if [ "$1" == "Gtk3" ]; then
 	SOLUTION=MonoMultiJack.Lnx.Gtk3.sln
 	OTHER=MonoMultiJack.Lnx.sln
-	cp -f MonoMultiJack.Linux/MonoMultiJack.Linux.Gtk3.config MonoMultiJack.Linux/app.config
 fi
 
 git submodule update --init --recursive
